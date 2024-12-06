76834b0704d01ee6646e27be6e1e53bd3a062f8c
Erin Bailey <146312546+Moosecapade@users.noreply.github.com>
2024-12-06 13:03:21 -0800

    Update lru_replacement.cpp

--- assign5/lru_replacement.cpp
+++ assign5/lru_replacement.cpp
@@ -10,32 +10,43 @@
 
 #include "lru_replacement.h"
 
-// TODO: Add your implementation here
+
 LRUReplacement::LRUReplacement(int num_pages, int num_frames)
-: Replacement(num_pages, num_frames)
-{
-    // TODO: Complete this constructor
-}
+: Replacement(num_pages, num_frames){}
 
-// TODO: Add your implementations for desctructor, touch_page, load_page, replace_page here
-LRUReplacement::~LRUReplacement()
-{
-    // TODO: Add necessary code here
-}
 
+LRUReplacement::~LRUReplacement(){}
+
+/*
 // Accesss a page alreay in physical memory
 void LRUReplacement::touch_page(int page_num)
 {
-    // TODO: Update your data structure LRU replacement
+
 }
+*/
 
 // Access an invalid page, but free frames are available
 void LRUReplacement::load_page(int page_num) {
-    // TODO: Update your data structure LRU replacement and pagetable
+    num_fault++;
+    //greatest_frame++;
+    page_table[page_num].frame_num = used_frames;	// used_frames doubles as a frame number recorder here
+    page_table[page_num].valid = true;		// make the new page valid
+    //need pr_table to push a pointer to this page
+    pr_table.push(&page_table[page_num]);
 }
 
 // Access an invalid page and no free frames are available
 int LRUReplacement::replace_page(int page_num) {
-    // TODO: Update your data structure LRU replacement and pagetable
+    num_fault++;
+    num_replace++;
+    PageEntry* temp = pr_table.top(); //temp holds the oldest page
+    pr_table.pop(); // delete oldest page from queue
+    temp->valid = false;
+    int freed_frame = temp->frame_num;  // contains the frame just freed
+    temp = nullptr; // drop temp
+    
+    page_table[page_num].valid = true;  // new page is valid
+    page_table[page_num].frame_num = freed_frame;   // new page has freed frame number
+
     return 0;
 }
